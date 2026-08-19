e4dd64c8c1f31d0aa1d25dfb7b129af3ef128aa3
SatriaAkbarRizki <sartiapl@gmail.com>
2026-08-19 11:36:16 +0700

    Update: change position card home

--- lib/widget/home/card_action_scan.dart
+++ lib/widget/home/card_action_scan.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CardActionScanHome extends StatelessWidget {
   const CardActionScanHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Card(
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 0.85,
+      ),
+      itemCount: 2,
+      itemBuilder: (context, index) {
+        return Card(
           elevation: 2,
           child: SizedBox(
             width: double.infinity,
@@ -21,57 +29,38 @@ class CardActionScanHome extends StatelessWidget {
                 Container(
                   width: 90,
                   height: 90,
-                  margin: EdgeInsets.only(bottom: 10),
+                  margin: const EdgeInsets.only(bottom: 10),
                   decoration: BoxDecoration(
                     color: Colors.blue,
                     borderRadius: BorderRadius.circular(5),
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: SvgPicture.asset(
+                      index == 0
+                          ? 'assets/icons/icon-image.svg'
+                          : 'assets/icons/icon-camera.svg',
+                      colorFilter: const ColorFilter.mode(
+                        Colors.white,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
                 ),
-
+                SizedBox(height: 10),
                 Text(
-                  'Camera Scan',
+                  index == 0 ? 'Image Scan' : 'Camera Scan',
                   style: Theme.of(context).textTheme.labelMedium,
                 ),
                 Text(
-                  'Use device camera',
+                  index == 0 ? 'Use Image from Gallery' : 'Use device camera',
                   style: Theme.of(context).textTheme.bodySmall,
                 ),
               ],
             ),
           ),
-        ),
-        Card(
-          elevation: 2,
-          child: SizedBox(
-            width: double.infinity,
-            height: 180,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 2,
-              children: [
-                Container(
-                  width: 90,
-                  height: 90,
-                  margin: EdgeInsets.only(bottom: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-
-                Text(
-                  'Camera Scan',
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
-                Text(
-                  'Use device camera',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
